7eb86dd2c6919c0ce91f40811dbcb20c7501d02b
nmrmasakazu <m.nomura.733@nitech.jp>
2021-01-30 12:18:51 +0900

    roleの固定

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -2,11 +2,16 @@ import * as express from "express";
 import { Container } from "inversify";
 import { container } from '../inversify.config';
 
+type roleType = 'admin' | 'user'
+
 const authMiddlewareFactory = (container: Container) => {
-    return (config: { role: string }) => {
+    return (config: { role: roleType }) => {
         return (req: express.Request, res: express.Response, next: express.NextFunction) => {
             console.log(req.cookies);
-            console.log(`Passing Middleware. Role is ${config.role}`);
+            const dummyRole = 'admins'
+            if (dummyRole === config.role) {
+                console.log(`Passing Middleware. Role is ${config.role}`);
+            }
             next();
             // res.status(403).end("Forbidden");
             // res.status(401).end("Unauthorized");
